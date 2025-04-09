01f29e1f953e3853aa6c5a9426f7fc17d8a60fb6
prannayj <39214786+prannayj@users.noreply.github.com>
2025-04-09 17:08:08 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: 'marquee 30s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
